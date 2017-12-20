e2de7f90857d54a6accd098d79f22e54ca8a4f8f
nancyUnitec <xin080124@hotmail.com>
2017-12-20 20:02:10 +1300

    add c project

---
+++ forC/forC/forCPP.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "forCPP.h"
+
+
+void withCC::showCallOrder()
+{
+	cout << "base function" << endl;
+}
+
+/*
+void subCC::showCallOrder()
+{
+	cout << "sub function" << endl;
+}
+*/
+
---
+++ forC/forC/forCPP.h
@@ -0,0 +1,34 @@
+﻿//using namespace std
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class withCC
+{
+public:
+	withCC() {}
+	withCC(const withCC&)
+	{
+    	cout << "withCC(withCC&)" << endl;
+	}
+
+	//virtual void showCallOrder();
+	void showCallOrder();
+};
+
+class subCC:public withCC
+{
+public:
+	//virtual void showCallOrder();
+	//void showCallOrder();
+};
+
+
+class T {
+public:
+	T() { cout << "constructor" << endl; }
+	~T() { cout << "destructor" << endl; }
+};
